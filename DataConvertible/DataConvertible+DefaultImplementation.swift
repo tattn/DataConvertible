5ba8f513b03f79e35511b189bdf7167f53700cea
Happy HumanPointer <45888860+SafelySwift@users.noreply.github.com>
2019-01-13 13:44:16 -0500

    Update DataConvertible+DefaultImplementation.swift

--- DataConvertible/DataConvertible+DefaultImplementation.swift
+++ DataConvertible/DataConvertible+DefaultImplementation.swift
@@ -1,22 +1,30 @@
-func _getJSON<T>(_ val: T, _ json: String = "", _ indent: Int = 1) -> String {
-    var json = json
+import Foundation
+
+func _getJSON<T>(_ val: T, _ indentString: String = "\t", _ indent: Int = 1) -> String {
+    var json = ""
     json.append("{")
-    let mirror = Mirror(reflecting: val)
-    for child in mirror.children {
-        guard let key = child.label else { continue }
-        json.append("\n")
-        json.append(contentsOf: repeatElement("\t", count: indent))
-        if let string = child.value as? String {
-            json.append("\"\(key)\":\"\(string)\"")
-        } else if let int = child.value as? Int {
-            json.append("\"\(key)\":\(int)")
-        } else if child.value is LosslessStringConvertible {
-            json.append("\"\(key)\":\"\(child.value)\"")
-        } else {
-            json.append("\"\(key)\":\(_getJSON(child.value, "", indent + 1))")
+    if let description = val as? LosslessStringConvertible {
+        json.append("\n\"value\":\"\(description)\"\n}")
+    } else {
+        let mirror = Mirror(reflecting: val)
+        for child in mirror.children {
+            guard let key = child.label else { continue }
+            json.append("\n")
+            for indent in repeatElement(indentString, count: indent) {
+                json.append(indent)
+            }
+            if let string = child.value as? String {
+                json.append("\"\(key)\":\"\(string)\"")
+            } else if let int = child.value as? Int {
+                json.append("\"\(key)\":\(int)")
+            } else if child.value is LosslessStringConvertible {
+                json.append("\"\(key)\":\"\(child.value)\"")
+            } else {
+                json.append("\"\(key)\":\(_getJSON(child.value, indentString, indent + 1))")
+            }
         }
+        json.append("\n\(String(repeating: indentString, count: indent - 1))}")
     }
-    json.append("\n\(String(repeating: "\t", count: indent - 1))}")
     return json
 }
 
